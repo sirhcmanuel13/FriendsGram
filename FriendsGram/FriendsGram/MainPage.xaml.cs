59a0b4eeb32bfd0958688754ad10e34419d553d3
Harvey Burgos <harveyburgos1@gmail.com>
2018-11-15 11:29:51 +0800

    Added PostPage

---
+++ FriendsGram/FriendsGram/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using FriendsGram.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FriendsGram
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+    
+        }
+
+    }
+}
